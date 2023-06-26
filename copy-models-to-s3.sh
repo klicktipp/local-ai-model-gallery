dcea155466f8d50fbcab4cfaa100845b1dbcb9fe
Rainer 'rei' Schuth <rainer.schuth@digital-results-international.com>
2023-06-26 17:45:50 -0500

    Follow redirects

--- copy-models-to-s3.sh
+++ copy-models-to-s3.sh
@@ -5,6 +5,6 @@ set -eu
 for model_url in $(cat *.yaml | grep "uri.*\.bin" | cut -d '"' -f2); do 
   model="${model_url##*/}"
   echo "=== Model: ${model} ==="
-  curl "${model_url}" | aws s3 cp - "s3://kt-ai-stack/local-ai/gpt4all.io/models/${model}"
+  curl -L "${model_url}" | aws s3 cp - "s3://kt-ai-stack/local-ai/gpt4all.io/models/${model}"
 done
 
