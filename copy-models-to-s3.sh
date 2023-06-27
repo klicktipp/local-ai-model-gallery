91a0d20058af6001c2315ab64681f67718ad3818
Rainer 'rei' Schuth <rainer.schuth@digital-results-international.com>
2023-06-26 22:04:27 -0500

    Replace URIs

--- copy-models-to-s3.sh
+++ copy-models-to-s3.sh
@@ -9,7 +9,7 @@ cd model-gallery
 for model_url in $(cat *.yaml | grep "uri.*\.bin" | cut -d '"' -f2); do 
   model="${model_url##*/}"
   echo "=== Model: ${model} ==="
-  curl -L "${model_url}" | aws s3 cp - "s3://kt-ai-stack/local-ai/gpt4all.io/models/${model}"
+  curl -L "${model_url}" | aws s3 cp - "https://kt-ai-stack.s3.amazonaws.com/local-ai/gpt4all.io/models/${model}"
 done
 
 rm -rf model-gallery
