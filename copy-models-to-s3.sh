e5df767cb54a9b43be395e6ac3c63f85938735cd
Rainer 'rei' Schuth <rainer.schuth@digital-results-international.com>
2023-06-26 21:29:41 -0500

    Fix script

--- copy-models-to-s3.sh
+++ copy-models-to-s3.sh
@@ -2,9 +2,14 @@
 
 set -eu
 
+git clone https://github.com/go-skynet/model-gallery
+
+cd model-gallery
+
 for model_url in $(cat *.yaml | grep "uri.*\.bin" | cut -d '"' -f2); do 
   model="${model_url##*/}"
   echo "=== Model: ${model} ==="
   curl -L "${model_url}" | aws s3 cp - "s3://kt-ai-stack/local-ai/gpt4all.io/models/${model}"
 done
 
+rm -rf model-gallery
